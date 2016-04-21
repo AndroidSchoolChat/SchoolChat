59b36353aecf7578d9c292c68e4c64112fc9c9c1
ggordo12 <ggordo12@hotmail.com>
2016-04-21 22:11:19 +0200

    Cambios realizados PARTE 2:    -Se han añadido los elementos  EditText y boton REGISTRAR y CANCELAR    -En los EditText se incluyen textos por defecto    -El boton REGISTRAR, NO REALIZA NINGUNA ACCION    -El boton CANCELAR, vuelve a la actividad principal
       El EditText de correo es un EditText de tipo Correo
    
       El EditText de Usuarios es un EditText normal
    
       El EditText de contraseña es un EditTExt de tipo Contraseña
    
    NOTA:  La segunda actividad es un prototipo, por si mas adelante se decide añadir codigo de alumno y codigo de profesor.

--- app/src/main/java/com/schoolchat/schoolchat/AltaUsuario.java
+++ app/src/main/java/com/schoolchat/schoolchat/AltaUsuario.java
@@ -1,9 +1,14 @@
 package com.schoolchat.schoolchat;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 /**
  * Created by Gabri on 20/04/2016.
@@ -14,10 +19,34 @@ import android.view.MenuItem;
 
 public class AltaUsuario extends AppCompatActivity {
 
+    /*  Declaracion de variables:  */
+
+    Button botonRegistrarse;
+    Button botonCancelar;
+
+    EditText ed_Em;
+    EditText ed_NomUsu;
+    EditText ed_Cont;
+
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_altausuario);
+
+        /*  Referenciando a los widgets  */
+
+
+        botonRegistrarse = (Button) findViewById(R.id.bt_Registrar);
+        botonCancelar = (Button) findViewById(R.id.bt_Cancelar);
+
+        ed_Em = (EditText) findViewById(R.id.ed_Email);
+        ed_NomUsu = (EditText) findViewById(R.id.ed_NombreUsuario);
+        ed_Cont = (EditText) findViewById(R.id.ed_Contraseña);
+
 
     }
 
@@ -46,5 +75,16 @@ public class AltaUsuario extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
+
+  /*  Accion a realizar al pulsar en REGISTRAR: Ir a la actividad MainActivity  */
+
+
+    public void onCancelar(View v){
+        Intent j = new Intent(this,MainActivity.class);
+        startActivity(j);
+    }
+
 
 }
