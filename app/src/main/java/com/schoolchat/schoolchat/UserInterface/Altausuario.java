06420c8a008bcf7027ce3d0d1c0c7f706a584883
ggordo12 <ggordo12@hotmail.com>
2016-05-09 21:51:49 +0200

    Cambios Gabriel:  -Diseño de la actividad AltaUsuario cambiado
     -Se intentara crear una contraseña de verificacion para registrarse como profesor.
     -Sera necesario crear un campo en FireBase a la misma altura que profesor y alumno para la verificacion
    
     NOTA: INTENTAR AÑADIR EL TITULO DE LA ACTIVIDAD

--- app/src/main/java/com/schoolchat/schoolchat/UserInterface/Altausuario.java
+++ app/src/main/java/com/schoolchat/schoolchat/UserInterface/Altausuario.java
@@ -23,53 +23,30 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Altausuario extends Activity {
-    private TabHost Pestañas;
-    //Pestaña para alumnos
+
     private Button botonRegistrarse;
     private Button botonCancelar;
     private EditText ed_Em;
     private EditText ed_NomUsu;
     private EditText ed_Cont;
-    //pestaña para profesores
-    private Button botonRegistrarseProfe;
-    private Button botonCancelarProfe;
-    private EditText ed_EmProfe;
-    private EditText ed_NomProfe;
-    private EditText ed_ContProfe;
-    //pestaña para administracion
-    private Button botonRegistrarseAdmin;
-    private Button botonCancelarAdmin;
-    private EditText ed_EmAdmin;
-    private EditText ed_NomAdmin;
-    private EditText ed_ContAdmin;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Firebase.setAndroidContext(this);     //AÑADIDO DEBIDO A QUE ES NECESARIO PARA QUE FUNCIONE FIREBASE
         setContentView(R.layout.activity_altausuario);
-        Pestañas=(TabHost)findViewById(R.id.tabHost);//lamada al tabhost
-        Pestañas.setup();//activacion del tabhost
-        botonCancelar = (Button) findViewById(R.id.bt_Cancelar);
-        //elementos para la pestaña alumnos
+
+
         botonRegistrarse = (Button) findViewById(R.id.bt_Registrarse);
+        botonCancelar = (Button) findViewById(R.id.bt_Cancelar);
+
         ed_Em = (EditText) findViewById(R.id.ed_Email);
         ed_NomUsu = (EditText) findViewById(R.id.ed_NombreUsuario);
         ed_Cont = (EditText) findViewById(R.id.ed_Contraseña);
-        //elementos para la pestaña profesores
-        botonRegistrarseProfe = (Button) findViewById(R.id.bt_RegistrarseProfe);
-        ed_EmProfe = (EditText) findViewById(R.id.ed_EmailProfe);
-        ed_NomProfe = (EditText) findViewById(R.id.ed_NombreProfe);
-        ed_ContProfe = (EditText) findViewById(R.id.ed_ContraseñaProfe);
 
-        //elementos para pestaña administracion
-        botonRegistrarseAdmin = (Button) findViewById(R.id.bt_RegistrarseAdmin);
-        ed_EmAdmin = (EditText) findViewById(R.id.ed_EmailAdmin);
-        ed_NomAdmin = (EditText) findViewById(R.id.ed_NombreAdmin);
-        ed_ContAdmin = (EditText) findViewById(R.id.ed_ContraseñaAdmin);
-        Pestañas.addTab(Pestañas.newTabSpec("tab1").setIndicator("alumnos",null).setContent(R.id.alumnos));
-        Pestañas.addTab(Pestañas.newTabSpec("tab2").setIndicator("profesores",null).setContent(R.id.profesores));
-        Pestañas.addTab(Pestañas.newTabSpec("tab3").setIndicator("administracion",null).setContent(R.id.administracion));
+
     }
+
 
 
     /*  Accion a realizar al pulsar en REGISTRAR: Ir a la actividad MainActivity
@@ -128,114 +105,8 @@ public class Altausuario extends Activity {
             });
         }
     }
-    public void onSignProfe(View v){
-        //variables para registro pasadas a string
-        String useremail=ed_EmProfe.getText().toString();
-        String userName=ed_NomProfe.getText().toString();
-        String userpassword=ed_ContProfe.getText().toString();
-        if(useremail.isEmpty()||userName.isEmpty()||userpassword.isEmpty()){
-            MostrarError("Asegurese de que todos los campos esten rellenos");
-        }else{
-            final Firebase registroUsuario=new Firebase(conexion.FIREBASE_SCHOOLCHAT);
-            final String finalUserEmail=useremail;
-            final String finalUserPassword=userpassword;
-            final String finalUserName=userName;
-            //crear el usuario
-            registroUsuario.createUser(useremail,userpassword,new Firebase.ValueResultHandler<Map<String,Object>>(){
 
 
-                @Override
-                public void onSuccess(Map<String, Object> stringObjectMap) {
-
-                    Toast.makeText(Altausuario.this,"ususario creado",Toast.LENGTH_SHORT).show();
-
-                    registroUsuario.authWithPassword(finalUserEmail,finalUserPassword,new Firebase.AuthResultHandler(){
-
-                        @Override
-                        public void onAuthenticated(AuthData authData) {
-                            Map<String,Object> map=new HashMap<String, Object>();
-                            map.put(conexion.NOMBRE,finalUserName);
-                            map.put(conexion.USER_EMAIL,finalUserEmail);
-                            map.put(conexion.CHILD_CONNECT,conexion.ESTADO_OFFLINE);
-
-
-                            Date fecha=new Date(); //CAMBIADO*GABRI
-                            map.put(conexion.FECHA,fecha);
-
-                            // registroUsuario.child("Usuarios: ").child(authData.getUid()).setValue(map); //CAMBIO*GABRI -- Esta linea añade en la pestaña DATA, los datos recogidos del nuevo usuario.
-
-                            registroUsuario.child("Profesores: ").child(authData.getUid()).setValue(map); //CAMBIADO PARA QUE SE MUESTRE EL NOMBRE DE USUARIO EN VEZ DE EL UID!!
-                        }
-
-                        @Override
-                        public void onAuthenticationError(FirebaseError firebaseError) {
-
-                        }
-                    });
-                }
-
-                @Override
-                public void onError(FirebaseError firebaseError) {
-                    //si el usuario tiene problemas con el registro se le notificara un error
-                    MostrarError(firebaseError.getMessage());
-                }
-            });
-        }
-    }
-    public void onSignAdmin(View v){
-        //variables para registro pasadas a string
-        String useremail=ed_EmAdmin.getText().toString();
-        String userName=ed_NomAdmin.getText().toString();
-        String userpassword=ed_ContAdmin.getText().toString();
-        if(useremail.isEmpty()||userName.isEmpty()||userpassword.isEmpty()){
-            MostrarError("Asegurese de que todos los campos esten rellenos");
-        }else{
-            final Firebase registroUsuario=new Firebase(conexion.FIREBASE_SCHOOLCHAT);
-            final String finalUserEmail=useremail;
-            final String finalUserPassword=userpassword;
-            final String finalUserName=userName;
-            //crear el usuario
-            registroUsuario.createUser(useremail,userpassword,new Firebase.ValueResultHandler<Map<String,Object>>(){
-
-
-                @Override
-                public void onSuccess(Map<String, Object> stringObjectMap) {
-
-                    Toast.makeText(Altausuario.this,"ususario creado",Toast.LENGTH_SHORT).show();
-
-                    registroUsuario.authWithPassword(finalUserEmail,finalUserPassword,new Firebase.AuthResultHandler(){
-
-                        @Override
-                        public void onAuthenticated(AuthData authData) {
-                            Map<String,Object> map=new HashMap<String, Object>();
-                            map.put(conexion.NOMBRE,finalUserName);
-                            map.put(conexion.USER_EMAIL,finalUserEmail);
-                            map.put(conexion.CHILD_CONNECT,conexion.ESTADO_OFFLINE);
-
-
-                            Date fecha=new Date(); //CAMBIADO*GABRI
-                            map.put(conexion.FECHA,fecha);
-
-                            // registroUsuario.child("Usuarios: ").child(authData.getUid()).setValue(map); //CAMBIO*GABRI -- Esta linea añade en la pestaña DATA, los datos recogidos del nuevo usuario.
-
-                            registroUsuario.child("Administracion: ").child(finalUserName).setValue(map); //CAMBIADO PARA QUE SE MUESTRE EL NOMBRE DE USUARIO EN VEZ DE EL UID!!
-                        }
-
-                        @Override
-                        public void onAuthenticationError(FirebaseError firebaseError) {
-
-                        }
-                    });
-                }
-
-                @Override
-                public void onError(FirebaseError firebaseError) {
-                    //si el usuario tiene problemas con el registro se le notificara un error
-                    MostrarError(firebaseError.getMessage());
-                }
-            });
-        }
-    }
     //metodo para crear mensajes de alerta de errores
     private void MostrarError(String error){
         //Create an AlertDialog to show error message
