2033869be1358758d2c28c453be323173c231a29
olopezcam <olopezcam@gmail.com>
2016-05-30 21:29:45 +0200

    Añadidos los iconos de ic_launcher.png

--- app/src/main/java/com/schoolchat/schoolchat/Adaptadores/AdaptadorUsuarios.java
+++ app/src/main/java/com/schoolchat/schoolchat/Adaptadores/AdaptadorUsuarios.java
@@ -2,7 +2,6 @@ package com.schoolchat.schoolchat.Adaptadores;
 
 import android.content.Context;
 import android.content.Intent;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
